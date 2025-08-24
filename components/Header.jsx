7c4e6671bdfb72db6201538ce6e4c84ee7dfead2
sudhanshu1711 <sudhanshu2679@gmail.com>
2025-08-24 11:46:38 +0530

    setup file for adding user data into table

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,8 +4,10 @@ import Link from 'next/link'
 import { Button } from './ui/button'
 import { PenBox } from 'lucide-react'
 import UserMenu from './user-menu'
+import { checkUser } from '@/lib/checkUser'
 
-const Header = () => {
+const Header = async() => {
+  await checkUser()
   return (
        <header className="container mx-auto">
        <nav className="py-6 px-4 flex justify-between items-center">
